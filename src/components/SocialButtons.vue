3356abff9c70fda5e6414c135601b677e6bdf041
Pavel Torbeev <pavel.torbeev@performi.ca>
2025-02-11 15:27:12 +0600

    Rework images

--- src/components/SocialButtons.vue
+++ src/components/SocialButtons.vue
@@ -7,44 +7,52 @@
 </template>
 
 <script>
-  export default {
-    name: 'SocialButtons',
-    data: function () {
-        return {
-          buttons: [{
-            id: 'instagram',
-            icon: './src/assets/img/instagram.png',
-            link: 'https://www.instagram.com/paveltorbeev/',
-          }, {
-            id: 'telegram',
-            icon: './src/assets/img/telegram.png',
-            link: 'https://t.me/ptorbeev',
-          },{
-            id: 'twitter',
-            icon: './src/assets/img/twitter.png',
-            link: 'https://twitter.com/glizer',
-          },{
-            id: 'google-plus',
-            icon: './src/assets/img/google-plus.png',
-            link: 'mailto:p.torbeev@gmail.com',
-          },{
-            id: 'vk',
-            icon: './src/assets/img/vk.png',
-            link: 'https://vk.com/glizer',
-          },{
-            id: 'github',
-            icon: './src/assets/img/github.png',
-            link: 'https://github.com/glizer',
-          },{
-            id: 'facebook',
-            icon: './src/assets/img/facebook.png',
-            link: 'https://www.facebook.com/torbeev/',
-          },{
-            id: 'linkedin',
-            icon: './src/assets/img/linkedin.png',
-            link: 'https://www.linkedin.com/in/glizer/',
-          }]
-        }
-    }
+import telegramIcon from '../assets//img/telegram.png';
+import twitterIcon from '../assets//img/twitter.png';
+import googlePlusIcon from '../assets//img/google-plus.png';
+import vkIcon from '../assets//img/vk.png';
+import githubIcon from '../assets//img/github.png';
+import facebookIcon from '../assets//img/facebook.png';
+import linkedinIcon from '../assets//img/linkedin.png';
+
+export default {
+  name: 'SocialButtons',
+  data() {
+    return {
+      buttons: [{
+        id: 'instagram',
+        icon: telegramIcon,
+        link: 'https://www.instagram.com/paveltorbeev/',
+      }, {
+        id: 'telegram',
+        icon: telegramIcon,
+        link: 'https://t.me/ptorbeev',
+      }, {
+        id: 'twitter',
+        icon: twitterIcon,
+        link: 'https://twitter.com/glizer',
+      }, {
+        id: 'google-plus',
+        icon: googlePlusIcon,
+        link: 'mailto:p.torbeev@gmail.com',
+      }, {
+        id: 'vk',
+        icon: vkIcon,
+        link: 'https://vk.com/glizer',
+      }, {
+        id: 'github',
+        icon: githubIcon,
+        link: 'https://github.com/glizer',
+      }, {
+        id: 'facebook',
+        icon: facebookIcon,
+        link: 'https://www.facebook.com/torbeev/',
+      }, {
+        id: 'linkedin',
+        icon: linkedinIcon,
+        link: 'https://www.linkedin.com/in/glizer/',
+      }]
+    };
   }
+};
 </script>
