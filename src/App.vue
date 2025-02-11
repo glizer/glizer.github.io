80f8750bc3db400b7aa4f70be8b5bcfe1d71e2e6
Pavel Torbeev <pavel.torbeev@performi.ca>
2025-02-11 13:58:24 +0600

    Replace by vue

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+  import SocialButtons from './components/SocialButtons.vue'
+</script>
+
+<template>
+  <main class="content">
+    <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+    <section id="home" class="align-items-center space-bg">
+
+      <div class="container">
+
+        <div class="intro">
+          <img src="./assets/img/avatar.png" alt="Pavel Torbeev" class="avatar-pic">
+
+          <h1>Pavel Torbeev</h1>
+          <span>I'm a frontend developer</span>
+
+          <social-buttons />
+        </div>
+      </div>
+
+    </section>
+    <section id="about" class="align-items-center">
+      <div class="container">
+        <h2 class="page-title">About Me</h2>
+        <div class="about">
+          <div class="avatar-pic big"><img src="./assets/img/picture.png"/></div>
+          <div class="description">
+            <div>
+              <h4>Hi, my name is Pavel and I am a frontend developer</h4>
+              <p>I love cool interfaces, mark-up, and pies</p>
+              <p>I love using my talents to bring more business to a worthy product.</p>
+              <p>I love productive work and hate bureaucracy</p>
+              <p>Please don't call me, I'm an introvert.</p>
+            </div>
+            <div class="tags"><span>HTML</span> <span>css</span><span>javascript</span><span>Vue.js</span><span>Playwright</span><span>git</span><span>Jest</span><span>webpack</span><span>sass</span><span>less</span><span>bootstrap</span><span>svg</span><span>d3</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+</template>
---
+++ src/components/SocialButtons.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="social-icons">
+    <li v-for="button in buttons" :key="button.id">
+      <a :href="button.link" target="_blank" :title="button.id"><img :src="button.icon"/></a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'SocialButtons',
+    data: function () {
+        return {
+          buttons: [{
+            id: 'instagram',
+            icon: './src/assets/img/instagram.png',
+            link: 'https://www.instagram.com/paveltorbeev/',
+          }, {
+            id: 'telegram',
+            icon: './src/assets/img/telegram.png',
+            link: 'https://t.me/ptorbeev',
+          },{
+            id: 'twitter',
+            icon: './src/assets/img/twitter.png',
+            link: 'https://twitter.com/glizer',
+          },{
+            id: 'google-plus',
+            icon: './src/assets/img/google-plus.png',
+            link: 'mailto:p.torbeev@gmail.com',
+          },{
+            id: 'vk',
+            icon: './src/assets/img/vk.png',
+            link: 'https://vk.com/glizer',
+          },{
+            id: 'github',
+            icon: './src/assets/img/github.png',
+            link: 'https://github.com/glizer',
+          },{
+            id: 'facebook',
+            icon: './src/assets/img/facebook.png',
+            link: 'https://www.facebook.com/torbeev/',
+          },{
+            id: 'linkedin',
+            icon: './src/assets/img/linkedin.png',
+            link: 'https://www.linkedin.com/in/glizer/',
+          }]
+        }
+    }
+  }
+</script>
