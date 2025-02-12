7c6439d9800e1bf4a2da7955e3c95419d1cdd064
Pavel Torbeev <pavel.torbeev@performi.ca>
2025-02-12 18:47:25 +0600

    Add auto-detect for language

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,12 @@ import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import ru from './locales/ru.json'
 
+const languages = ['ru', 'en'],
+    defaultLang = languages[0],
+    lang = (navigator.language || navigator.userLanguage).substr(0,2) || defaultLang;
+
 export const i18n = createI18n({
     legacy: false,
-    locale: 'ru',
+    locale: languages.includes(lang) ? lang : defaultLang,
     messages: { en, ru }
 })
