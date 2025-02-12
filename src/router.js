3eebe41064aed245d1e78856ec7b3846c531c37b
Pavel Torbeev <pavel.torbeev@performi.ca>
2025-02-12 18:37:35 +0600

    Image fixes

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from './components/HomePage.vue'
 import { i18n } from './i18n'
 import { nextTick } from 'vue'
@@ -7,18 +7,18 @@ const supportedLocales = ['ru', 'en']
 
 const routes = [
     { path: '/:locale(en|ru)', component: HomePage },
-    { path: '/:pathMatch(.*)*', redirect: `/${supportedLocales[0]}/` }
+    { path: '/:pathMatch(.*)*', redirect: `${supportedLocales[0]}` }
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
 router.beforeEach(async (to, from, next) => {
     const locale = to.params.locale
     if (!supportedLocales.includes(locale)) {
-        return next(`/${supportedLocales[0]}/`)
+        return next(`${supportedLocales[0]}`)
     }
 
     if (i18n.global.locale.value !== locale) {
